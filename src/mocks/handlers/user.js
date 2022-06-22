d9c6c4b56d687e78c16ce8e0d5062a3ad22ae09b
Ilia Mitvichin <ilia.mitvichin@kallidus.com>
2022-06-22 15:52:13 +0300

    added eslint

--- src/mocks/handlers/user.js
+++ src/mocks/handlers/user.js
@@ -1,7 +1,7 @@
 import { rest } from 'msw';
 
-const getUser = rest.get('/user', (req, res, ctx) => {
-  return res(ctx.json({ name: 'Ilia', familyName: 'Mitvichin' }));
-});
+const getUser = rest.get('/user', (req, res, ctx) =>
+  res(ctx.json({ name: 'Ilia', familyName: 'Mitvichin' }))
+);
 
 export const userHandlers = [getUser];
